19978db90a83b247255fa8e4206fdd3633e88a61
DamianDTM <69706528+Yolzky@users.noreply.github.com>
2021-11-15 13:17:32 -0500

    added first view of welcome client and worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ClientComponent} from "../components/client/client.component";
+import {CleaningWorkerComponent} from "../components/cleaning-worker/cleaning-worker.component";
+import {WelcomeComponent} from "../components/welcome/welcome.component";
+import {MatCardModule, MatCardTitle} from "@angular/material/card";
+import {MatButton, MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    CleaningWorkerComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
